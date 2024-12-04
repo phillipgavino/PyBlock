55ee838a1b055a32936b840ed189c9fa417c02e0
connorhennig8 <connorhennig@utexas.edu>
2024-12-03 23:24:57 -0600

    Voxel Code has Support now, GUI is much more friendly, added a part count list.

--- mainGUI.py
+++ mainGUI.py
@@ -1,7 +1,15 @@
 from PyBlockGUI import *
+from stl_voxel_layer_converter import *
 
 if __name__ == "__main__":
 
-    brick_data = [[0],[1,0],[2,1],[1,1],[0],[2,0]]
+    # brick_data = [[0],[1,0],[2,1],[1,1],[0],[2,0]]
+    # # brick_data = [[1,0],[2,1],[1,1],[2,0]]
 
-    pyblock_instuction_GUI(brick_data)
+    # pyblock_instuction_GUI(brick_data)
+
+    layer_data = stl_to_voxel_layer_data('3DBenchy.stl')
+
+    for layer in layer_data:
+        for row in layer:
+            print(row)
